7667707f75984c41168574b6d989931c5697b466
codelabite <chukwusahpeter@gmail.com>
2021-05-13 14:57:55 +0100

    perfect API

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
+
+const bodyParser = require("body-parser");
+const fileRoutes = require("./routes/file-upload-routes");
+const path = require("path");
+
 const DB = "mongodb://localhost:bestAPI";
-const DB_ONLINE = " ";
+const DB_ONLINE =
+  "mongodb+srv://shotkode:shotkode@cluster0.2kfdg.mongodb.net/shotkodeDB?retryWrites=true&w=majority";
 
 const app = express();
 
 mongoose
-  .connect(DB, {
+  .connect(DB_ONLINE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -18,6 +24,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api", fileRoutes.routes);
 
 app.get("/", async (req, res) => {
   res.status(200).send("API is ready for consumption");
